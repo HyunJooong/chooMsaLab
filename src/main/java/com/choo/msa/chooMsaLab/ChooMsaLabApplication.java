e77644a862609a5eba3f16e97d3b56e8acf722dd
chooHyun <hjchoo19950327@gmail.com>
2026-03-10 21:29:03 +0900

    Initial commit : Sptring Boot MSA personal prject

---
+++ src/main/java/com/choo/msa/chooMsaLab/ChooMsaLabApplication.java
@@ -0,0 +1,13 @@
+package com.choo.msa.chooMsaLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooMsaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChooMsaLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/choo/msa/chooMsaLab/ChooMsaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choo.msa.chooMsaLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChooMsaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
